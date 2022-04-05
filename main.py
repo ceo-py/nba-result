1e8fcb912f0854e78452e3c1edb4efb2b2d9d9ac
ceo-py <87097003+ceo-py@users.noreply.github.com>
2022-04-05 15:52:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import time
 import urllib.request
 import datetime
 
-
 client = commands.Bot(command_prefix="!", help_command=None)
 TOKEN = ("YOUR DISCORD BOT TOKEN HERE")
 DISCORD_CHANNEL_NAME = "nba-result"
@@ -23,51 +22,38 @@ def getdata(url):
 async def nba(ctx):
     while True:
         today = datetime.date.today()
-        Previous_Date = today - datetime.timedelta(days=1)
-        print(Previous_Date)
-        link_game_name = list()
-        odd_name = list()
-        even_name = list()
-        odd_score = list()
-        even_score = list()
-        htmldata = getdata(f"https://sports.yahoo.com/nba/scoreboard/?confId=&dateRange={Previous_Date}&schedState=")
+        previous_date = today - datetime.timedelta(days=1)
+        link_game_name, odd_name, even_name, odd_score, even_score = list(), list(), list(), list(), list()
+        htmldata = getdata(f"https://sports.yahoo.com/nba/scoreboard/?confId=&dateRange={previous_date}&schedState=")
         soup = BeautifulSoup(htmldata, 'html.parser')
         embed = discord.Embed(
             title=f"NBA Results {today}",
-            colour=discord.Colour.blue()
-        )
-
+            colour=discord.Colour.blue())
         for nu, link_game in enumerate(soup.find_all('a', attrs={'href': re.compile("/nba/")})):
             if nu > 11:
                 link_game_name.append(link_game.get('href'))
-
-
         for space, (name, score) in enumerate(zip(soup.find_all(class_="YahooSans Fw(700)! Fz(14px)!"),
-                               soup.find_all(class_="YahooSans Fw(700)! Va(m) Fz(24px)!"))):
+                                                  soup.find_all(class_="YahooSans Fw(700)! Va(m) Fz(24px)!"))):
             if (space % 2) == 0:
                 even_name.append(name.get_text())
                 even_score.append(score.get_text())
             else:
                 odd_name.append(name.get_text())
                 odd_score.append(score.get_text())
-
-        for name_o, score_o, name_e, score_e, game_url in zip(odd_name, odd_score, even_name, even_score, link_game_name):
+        for name_o, score_o, name_e, score_e, game_url in zip(odd_name, odd_score, even_name, even_score,
+                                                              link_game_name):
+            html = urllib.request.urlopen(f"https://www.youtube.com/results?search_query={game_url}")
+            video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+            game_highlight = f"https://www.youtube.com/watch?v={video_ids[0]}"
             if int(score_o) > int(score_e):
-                html = urllib.request.urlopen(f"https://www.youtube.com/results?search_query={game_url}")
-                video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-                game_highlight = (f"https://www.youtube.com/watch?v={video_ids[0]}")
-                embed.add_field(name=f"**{name_o} : {score_o}**\n~~{name_e} : {score_e}~~",
-                                value=f"[More Info]({url_start}{game_url})\n[Highlights]({game_highlight})",
-                                inline=True)
+                score_show = f"**{name_o} : {score_o}**\n~~{name_e} : {score_e}~~"
             else:
-                html = urllib.request.urlopen(f"https://www.youtube.com/results?search_query={game_url}")
-                video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-                game_highlight = (f"https://www.youtube.com/watch?v={video_ids[0]}")
-                embed.add_field(name=f"~~{name_o} : {score_o}~~\n**{name_e} : {score_e}**",
-                                value=f"[More Info]({url_start}{game_url})\n[Highlights]({game_highlight})",
-                                inline=True)
-
+                score_show = f"~~{name_o} : {score_o}~~\n**{name_e} : {score_e}**"
+            embed.add_field(name=score_show,
+                            value=f"[More Info]({url_start}{game_url})\n[Highlights]({game_highlight})",
+                            inline=True)
         await ctx.send(embed=embed)
-        time.sleep(86400) # time in seconds when you want to give you the results again 86400 is 24 hours
+        time.sleep(86400)  # time in seconds when you want to give you the results again 86400 is 24 hours
+
 
 client.run(TOKEN)
