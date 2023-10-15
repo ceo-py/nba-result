e827f540f7feb7146b947cedaf4453e4df1c0fee
ceo-py <sfmf@abv.bg>
2023-10-15 20:54:34 +0300

    Better emoji search only on last word

--- emojis/find_emojis.py
+++ emojis/find_emojis.py
@@ -1,5 +1,5 @@
 async def find_emojis(ctx, look_for_emoji: str) -> str:
     for emoji in ctx.guild.emojis:
-        if look_for_emoji in emoji.name:
+        if look_for_emoji.split()[-1] in emoji.name:
             return str(emoji)
     return ""
